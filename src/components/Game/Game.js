52af4f13a33c1d93218efe31e4f2ee9aa1496764
David Ball <david.ball.05@gmail.com>
2024-01-01 20:01:58 -0500

    Exercise 2: Guess List

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
+import GuessList from "../GuessList/GuessList";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,9 +11,16 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  function onGuess(guess) {
+    setGuesses([...guesses, guess]);
+  }
+
   return (
     <>
-      <GuessInput></GuessInput>
+      <GuessList guesses={guesses} />
+      <GuessInput onGuess={onGuess} />
     </>
   );
 }
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,22 +1,25 @@
 import React from "react";
 
-function GuessInput() {
+function GuessInput({ onGuess }) {
   const [guess, setGuess] = React.useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log("Guess", guess);
+    console.log({ guess });
     setGuess("");
+    onGuess(guess);
   }
 
   return (
-    <form class="guess-input-wrapper" onSubmit={(evt) => handleSubmit(evt)}>
+    <form className="guess-input-wrapper" onSubmit={(evt) => handleSubmit(evt)}>
       <label htmlFor="guess-input">Enter Guess</label>
       <input
+        required
         id="guess-input"
         pattern="[A-Za-z]{5}"
-        maxLength="5"
+        minLength={5}
+        maxLength={5}
         value={guess}
         onInput={(evt) => setGuess(evt.target.value.toUpperCase())}
       ></input>
